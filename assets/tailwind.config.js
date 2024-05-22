0ea7c2de41df16be7b48d93514a879a5d1c90c07
PJ Concepcion <32558422+pjconcepcion@users.noreply.github.com>
2024-05-22 21:09:59 +0800

    added welcome and about sections

---
+++ assets/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+  theme: {
+    fontSize: {
+      xs: 20,
+      sm: 24,
+      md: 30,
+      lg: 32,
+      xl: 36,
+      '2xl': 48,
+      '3xl': 80,
+      '4xl': 120
+    },
+    colors: {
+      gray: '#E4E4E4',
+      black: '#535353',
+      purple: '#7D34F4',
+      red: '#EE5757',
+      blue: '#2C9EF0',
+    },
+    fontFamily: {
+      sans: 'Roboto'
+    },
+    extend: {
+      padding: {
+        '120': '120px'
+      },
+      width: {
+        '844': '844px'
+      }
+    }
+  }
+}
